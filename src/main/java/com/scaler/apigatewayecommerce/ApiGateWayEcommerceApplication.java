ba30091430614d591732ff04c150942c59e00a2e
Bhaskar Sunchu <sunchubhaskar19@gmail.com>
2024-09-23 21:38:48 +0530

    First Commit

---
+++ src/main/java/com/scaler/apigatewayecommerce/ApiGateWayEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewayecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/apigatewayecommerce/ApiGateWayEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.apigatewayecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
